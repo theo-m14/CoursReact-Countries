a47e16ae16a0ede27919994d4d765298278f0916
Théo Michel <theo.michel2@outlook.fr>
2022-05-07 00:25:12 +0200

    vérification formulaire + style conditionnel

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../components/Logo";
 import Navigation from "../components/Navigation";
 
 const Blog = () => {
+  const [content, setContent] = useState("");
+  const [error, setError] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (content.length < 140) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+  };
+
   return (
     <div className="blog-container">
       <Logo />
       <Navigation />
       <h1>Blog</h1>
-      <form>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <input type="text" name="" id="" placeholder="Nom" />
-        <textarea placeholder="Message"></textarea>
+        <textarea
+          style={{ border: error ? "1px solid red" : "1px solid #61dafb" }}
+          placeholder="Message"
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+        {error && <p>Veuillez écrire un minimum de 140 caractères</p>}
         <input type="submit" value="Envoyer" />
       </form>
     </div>
