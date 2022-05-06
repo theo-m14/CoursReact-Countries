a39b42e551a2266bf8d061303004ad650c0e0805
Théo Michel <theo.michel2@outlook.fr>
2022-05-06 20:29:49 +0200

    Router et navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        {/* le path * correspond au routes inconnu de celles déclarés au dessus */}
+        <Route path="*" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,86 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation></Navigation>
+      <h1>A propos</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+        reprehenderit commodi est voluptas voluptatum, ipsam laborum tempora
+        enim, quae, harum doloribus dolor eius distinctio iusto non ullam earum
+        laudantium libero. Veniam laudantium perferendis inventore similique
+        error nam, debitis, eum distinctio voluptatibus adipisci excepturi sit
+        ullam voluptatum recusandae, tempora temporibus consectetur quaerat
+        animi. Commodi quas placeat optio sint, nemo soluta sequi. Cumque
+        obcaecati facere ducimus, inventore mollitia praesentium distinctio
+        odit? Atque quam, autem ad aut voluptatum veritatis nobis, id aperiam
+        eum odit, eveniet sint explicabo quod facere! Commodi est iste
+        consequuntur. Vel eum cum veritatis? Nobis culpa aspernatur distinctio
+        nulla odit. Sequi modi libero dolor eos sapiente. Quae optio obcaecati
+        id tempora eligendi exercitationem sapiente. At earum dolores et fugiat
+        quo. Ipsa sunt eligendi repellendus quidem velit nesciunt, ipsam quasi
+        rem voluptate voluptas! Sit dolor, quos excepturi quas vero harum
+        repudiandae explicabo doloremque quae delectus dolorem sint sed a
+        officiis eos! Deleniti laudantium, non cupiditate placeat sed in
+        voluptas maiores at nemo debitis praesentium vero, consequatur dolores
+        beatae tenetur pariatur omnis earum explicabo fuga autem, recusandae
+        possimus nobis? Ea, blanditiis eveniet. Temporibus quisquam porro
+        nesciunt sed, reiciendis, incidunt recusandae in nihil doloremque modi
+        assumenda sapiente repellat adipisci labore, unde pariatur? Ratione
+        doloremque dignissimos quaerat consequuntur cumque minima sapiente
+        asperiores quidem sunt? Aliquid sunt placeat sit aut ipsum distinctio
+        quos minima maxime error a! Voluptates, perferendis ad ex iure labore,
+        aliquid id numquam doloremque cupiditate dolores nisi dolore modi!
+        Omnis, architecto tenetur! Blanditiis magni laudantium deleniti
+        similique unde? Quisquam molestias tempore nihil facere blanditiis,
+        maiores ratione porro, eaque temporibus eveniet debitis facilis
+        mollitia, esse quidem repudiandae corporis dicta odit cumque architecto
+        aspernatur. Quo voluptatem alias laudantium tenetur veritatis ullam
+        voluptates, dignissimos omnis dolor officia quisquam officiis vero aut
+        consequatur, corrupti eos blanditiis impedit dolore quas neque? Deleniti
+        ex placeat nisi libero sint.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+        reprehenderit commodi est voluptas voluptatum, ipsam laborum tempora
+        enim, quae, harum doloribus dolor eius distinctio iusto non ullam earum
+        laudantium libero. Veniam laudantium perferendis inventore similique
+        error nam, debitis, eum distinctio voluptatibus adipisci excepturi sit
+        ullam voluptatum recusandae, tempora temporibus consectetur quaerat
+        animi. Commodi quas placeat optio sint, nemo soluta sequi. Cumque
+        obcaecati facere ducimus, inventore mollitia praesentium distinctio
+        odit? Atque quam, autem ad aut voluptatum veritatis nobis, id aperiam
+        eum odit, eveniet sint explicabo quod facere! Commodi est iste
+        consequuntur. Vel eum cum veritatis? Nobis culpa aspernatur distinctio
+        nulla odit. Sequi modi libero dolor eos sapiente. Quae optio obcaecati
+        id tempora eligendi exercitationem sapiente. At earum dolores et fugiat
+        quo. Ipsa sunt eligendi repellendus quidem velit nesciunt, ipsam quasi
+        rem voluptate voluptas! Sit dolor, quos excepturi quas vero harum
+        repudiandae explicabo doloremque quae delectus dolorem sint sed a
+        officiis eos! Deleniti laudantium, non cupiditate placeat sed in
+        voluptas maiores at nemo debitis praesentium vero, consequatur dolores
+        beatae tenetur pariatur omnis earum explicabo fuga autem, recusandae
+        possimus nobis? Ea, blanditiis eveniet. Temporibus quisquam porro
+        nesciunt sed, reiciendis, incidunt recusandae in nihil doloremque modi
+        assumenda sapiente repellat adipisci labore, unde pariatur? Ratione
+        doloremque dignissimos quaerat consequuntur cumque minima sapiente
+        asperiores quidem sunt? Aliquid sunt placeat sit aut ipsum distinctio
+        quos minima maxime error a! Voluptates, perferendis ad ex iure labore,
+        aliquid id numquam doloremque cupiditate dolores nisi dolore modi!
+        Omnis, architecto tenetur! Blanditiis magni laudantium deleniti
+        similique unde? Quisquam molestias tempore nihil facere blanditiis,
+        maiores ratione porro, eaque temporibus eveniet debitis facilis
+        mollitia, esse quidem repudiandae corporis dicta odit cumque architecto
+        aspernatur. Quo voluptatem alias laudantium tenetur veritatis ullam
+        voluptates, dignissimos omnis dolor officia quisquam officiis vero aut
+        consequatur, corrupti eos blanditiis impedit dolore quas neque? Deleniti
+        ex placeat nisi libero sint.
+      </p>
+    </div>
+  );
+};
+
+export default About;
